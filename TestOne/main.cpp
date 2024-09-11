63063b012e897ecd2e5e1c2cd7b049f4231fe067
YuC21 <bwlhaictk@gmail.com>
2024-09-11 19:31:29 +0900

    structヘッダーを追加。変数としても使い始めたよ

--- TestOne/main.cpp
+++ TestOne/main.cpp
@@ -1,19 +1,26 @@
 #include <stdio.h>
+#include "sturct.h"
 
 int main(void)
 {
 	int A = 0;
 	char B;
+	CUO s;
 
 	printf("これは練習用プログラムである");
 
 	printf("ブランチ作成後に追加した行");
+
+	s.q = 1;
+	s.s = 4;
 
 	if (A == 0) {
 		printf("Aは0,でもAは1かも？ , Bはどう?");
 	}
 
 	printf("新しく追記したぞ。char Bを足した");
+
+	printf("s.q = %d , s.s = %d",s.q,s.s);
 
 	return 0;
 }
---
+++ TestOne/sturct.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdio.h>
+
+//新しくstruct用のヘッダーを作ったぞ
+
+struct CUO
+{
+	int q = 1;
+	int s = 0;
+};
