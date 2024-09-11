8ea6ce59f1d8e731d05b7b9d8ad72ff7d92a0cd5
YuC21 <bwlhaictk@gmail.com>
2024-09-11 18:57:23 +0900

    char Bを足した。printfにも追記しておいた

--- TestOne/main.cpp
+++ TestOne/main.cpp
@@ -3,14 +3,17 @@
 int main(void)
 {
 	int A = 0;
+	char B;
 
 	printf("これは練習用プログラムである");
 
 	printf("ブランチ作成後に追加した行");
 
 	if (A == 0) {
-		printf("Aは0,でもAは1かも？");
+		printf("Aは0,でもAは1かも？ , Bはどう?");
 	}
+
+	printf("新しく追記したぞ。char Bを足した");
 
 	return 0;
 }
